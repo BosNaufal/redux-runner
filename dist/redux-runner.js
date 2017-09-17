35cb9f9a2714c032ecb04d1640c099dc64ce5510
Naufal <bosnaufalemail@gmail.com>
2017-09-17 16:15:54 +0700

    Add Build

--- dist/redux-runner.js
+++ dist/redux-runner.js
@@ -2,7 +2,7 @@
  * Copyright (c) Naufal Rabbani (http://github.com/BosNaufal)
  * Licensed Under MIT (http://opensource.org/licenses/MIT)
  * 
- * Redux Runner @ Version 0.0.0
+ * Redux Runner @ Version 0.0.1
  * 
  */
 (function webpackUniversalModuleDefinition(root, factory) {
